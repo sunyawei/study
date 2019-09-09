ae45de7366dca609936f53f839211f2dbc48fc9f
sunyawei <642948856@qq.com>
2019-09-09 15:56:40 +0800

    update

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,9 +1,52 @@
 package com.test;
 
 public class Test {
-	
+    static final Object object = new Object();
+
 	public static void main(String[] args) {
-		System.out.println("11");
-	}
+		
+		Thread thread1 = new Thread(
+				new Runnable() {
+			    @Override
+			      public void run() {
+					synchronized(object){
+						 for(int i=0;i<100;i++){
+							 System.out.println(Thread.currentThread().getName()+"，数值："+(i*2));
+							 object.notify();
+							 try {
+								object.wait();
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						    
+						 }
+					}
+				   }
+				});
+		
+		Thread thread2 = new Thread(
+				new Runnable() {
+			    @Override
+			      public void run() {
+					synchronized(object){
+						 for(int i=0;i<100;i++){
+							 System.out.println(Thread.currentThread().getName()+"，数值："+(i*2+1));
+							 object.notify();
+							 try {
+								object.wait();
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						    
+						 }
+					}
+				   }
+				});
+		thread1.start();
+		thread2.start();
+
+	   }
 
 }
